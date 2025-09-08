f9378f2f0087cf19a2cae2c18ea6df8dcae3bb7a
Gustavo Frainer <guga.frainer1@gmail.com>
2025-09-08 22:10:38 +0000

    Projeto - Commit 11

--- Sources/Aula_1/main.swift
+++ Sources/Aula_1/main.swift
@@ -1,5 +1,30 @@
 import Foundation
 
-class Main {
-    
-}
+let aluno1 = Aluno(
+    matricula: "GJK3041", 
+    nome: "Gustavo Braz Frainer", 
+    email: "guga.frainer1@gmail.com", 
+    plano: PlanoMensal())
+print()
+print("Aluno:", aluno1.getDescricao())
+
+let instrutor1 = Instrutor(
+    especialidade: "Treino de Superior",
+    nome: "Julio Balestrin",
+    email: "julio.balestrin@gmail.com")
+print("Instrutor:", instrutor1.getDescricao())
+print()
+
+let aluno2 = Aluno(
+    matricula: "KTD3571",
+    nome: "Kemuel de Matos",
+    email: "kemueldematos@gmail.com",
+    plano: PlanoAnual())
+print("Aluno:", aluno2.getDescricao())
+
+let instrutor2 = Instrutor(
+    especialidade: "Artes Marciais",
+    nome: "Charles do Bronx",
+    email: "charlesdobronx@gmail.com")
+print("Instrutor:", instrutor2.getDescricao())
+print()
