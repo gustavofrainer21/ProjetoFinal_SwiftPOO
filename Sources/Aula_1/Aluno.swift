5197bffa937c0b61625e1abeec5b9fc979d23ae1
Gustavo Frainer <guga.frainer1@gmail.com>
2025-09-08 19:13:33 +0000

    Projeto - Commit 8

--- Sources/Aula_1/Aluno.swift
+++ Sources/Aula_1/Aluno.swift
@@ -7,6 +7,7 @@ class Aluno: Pessoa {
         self.matricula = matricula
         self.plano = plano
         super.init(nome: nome, email: email)
+        self.nivel = .iniciante
     }
 
     override func getDescricao() -> String {
