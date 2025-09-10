ce783552ebcc1c2b726e71106f0e2c0607c3583d
Gustavo Frainer <guga.frainer1@gmail.com>
2025-09-10 18:14:38 +0000

    Projeto - Commit 24

--- Sources/Aula_1/Academia.swift
+++ Sources/Aula_1/Academia.swift
@@ -1,5 +1,27 @@
 import Foundation
 
 class Academia {
+    let nomeAcademia: String
+    private var alunosMatriculados: [String : Aluno] = [:]
+    private var instrutoresContratados: [String : Instrutor] = [:]
+    private var aparelhos [Aparelho] = []
+    private var aulasDisponiveis [Aula] = []
 
+    init(nomeAcademia: String) {
+        self.nomeAcademia = nomeAcademia
+    }
+
+    func adicionarAparelho(_ aparelho: Aparelho) {
+        aparelhos.append(aparelho)
+    }
+
+    func adicionarAula(_ aula: Aula) {
+        aulasDisponiveis.append(aula)
+    }
+
+    func contratarInstrutor(_ instrutor: Instrutor) {
+        instrutoresContratados[instrutor.email] = instrutor
+    }
+
+    
 }
